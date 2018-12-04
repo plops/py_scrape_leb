4178eb8bfced4bdcc7ba0869be5f06a2511b2c94
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-12-04 08:04:00 +0100

    i think i need a yield instruction

--- gen.lisp
+++ gen.lisp
@@ -2,10 +2,11 @@
   (ql:quickload :cl-py-generator))
 
 (in-package :cl-py-generator)
-
+;; https://doc.scrapy.org/en/latest/intro/tutorial.html
 ;; cd ~/stage/py_scrape_leb/source ; scrapy startproject tutorial
 ;; cd ~/stage/py_scrape_leb/source/tutorial ; scrapy crawl quotes
-
+;; use selectorgadget.com bookmarklet to find selectors
+;; learn xpath, select link that contains text "next page"
 (let ((code
        `(do0
 	 (imports (sys
@@ -21,14 +22,10 @@
 		      ("yield scrapy.Request" :url url :callback
 					      self.parse)))
 		(def parse (self response)
-		  (setf page (aref (response.url.split (string "/"))
-				   -2)
-			fn (dot (string "/dev/shm/quotes-{}.html")
-				(format page))
-			f (open fn (string "wb")))
-		  (f.write response.body)
-		  (f.close)
-		  (self.log (dot (string "saved file {}.")
-				 (format fn)))))
+		  (for (quote (response.css (string "div.quote")))
+		       "yield"
+		       (dict ((string text) (dot (quote.css (string
+							     "span.text::text"))
+						 (extract_first)))))))
 	 )))
   (write-source "/home/martin/stage/py_scrape_leb/source/tutorial/tutorial/spiders/quotes_spider" code))
--- source/tutorial/tutorial/spiders/quotes_spider.py
+++ source/tutorial/tutorial/spiders/quotes_spider.py
@@ -3,23 +3,13 @@ import scrapy
 import pandas as pd
 import pathlib
 
+
 class QuotesSpider(scrapy.Spider):
-    name="quotes"
+    name = "quotes"
 
-    def start_requests(self):
-        urls=["http://quotes.toscrape.com/page/1/", "http://quotes.toscrape.com/page/2/"]
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
+    start_urls = ["http://quotes.toscrape.com/page/1/",
+                  "http://quotes.toscrape.com/page/2/"]
 
     def parse(self, response):
-        page=response.url.split("/")[-2]
-        fn="/dev/shm/quotes-{}.html".format(page)
-        f=open(fn, "wb")
-
-        f.write(response.body)
-        f.close()
-        self.log("saved file {}.".format(fn))
-
-
+        for quote in response.css("div.quote"):
+            yield {("text"): (quote.css("span.text::text").extract_first())}
